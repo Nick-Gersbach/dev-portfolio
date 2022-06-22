69fed71dba9ac8ba27d43f364f596e5810c6b72e
Nicholas Gersbach <54191233+Nick-Gersbach@users.noreply.github.com>
2022-06-22 07:57:12 -0700

    other project set

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const body = document.querySelector("body");
 const slider = document.querySelector(".slider");
 const waiveAnimation = document.querySelector(".wave");
+const otherPorjectTitle = document.querySelector(".other-project-title-text");
 
 slider.addEventListener("click", activateDarkMode);
 waiveAnimation.addEventListener("click", waiveOnClick);
 
 function activateDarkMode() {
   body.classList.toggle("dark-mode");
+  otherPorjectTitle.classList.add("dark-mode-text");
 }
 
 function waiveOnClick() {
