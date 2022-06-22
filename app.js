833f2130f4e5988b0cb1b1481d2d4d6714e0c70a
Nicholas Gersbach <54191233+Nick-Gersbach@users.noreply.github.com>
2022-06-22 14:46:34 -0700

    darkmode fixed

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const body = document.querySelector("body");
 const slider = document.querySelector(".slider");
 const waiveAnimation = document.querySelector(".wave");
-const otherPorjectTitle = document.querySelector(".other-project-title-text");
+const otherPorjectTitle = document.querySelectorAll(
+  ".other-project-title-text"
+);
 
 slider.addEventListener("click", activateDarkMode);
 waiveAnimation.addEventListener("click", waiveOnClick);
 
 function activateDarkMode() {
   body.classList.toggle("dark-mode");
-  otherPorjectTitle.classList.add("dark-mode-text");
+  for (const title of otherPorjectTitle) {
+    title.classList.add("dark-mode-text");
+  }
 }
 
 function waiveOnClick() {
